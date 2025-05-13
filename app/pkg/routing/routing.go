b40131cd3c458cc5fbcd8c8cd38544bd0f009d2e
Brandon <badamsva@gmail.com>
2025-05-13 01:22:52 -0400

    Try to handle URLS with multiple slashes.
    This is broken will need to fix later

--- app/pkg/routing/routing.go
+++ app/pkg/routing/routing.go
@@ -3,8 +3,13 @@ package routing
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
+type Context {
+	
+}
+// The string is the request string
 type HandlerFunc func(net.Conn)
 
 // Router is a dictionary of strings and handler functions
@@ -16,12 +21,15 @@ func (r *Router) AssignHandler(path string, handler HandlerFunc) {
 	r.routes[path] = handler
 }
 
-func (r *Router) Serve(conn net.Conn, path string) {
-	if handler, ok := r.routes[path]; ok {
-		handler(conn)
-	} else {
-		fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+func (r *Router) Serve(conn net.Conn, dataRecieved string) {
+	fullPath := strings.Split(dataRecieved, " ")[1]
+	for routePath, routeFunc := range r.routes {
+		if fullPath == routePath || strings.HasPrefix(fullPath, routePath+"/") {
+			routeFunc(conn, &dataRecieved)
+			return
+		}
 	}
+	fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
 }
 
 func NewRouterMap() *Router {
