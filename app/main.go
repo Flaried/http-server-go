4e7d5d3ba103e4a1a7ee15340638d9fa97b32aa6
Brandon <badamsva@gmail.com>
2025-05-12 20:03:28 -0400

    Respond to request
    Remove useless prints and stuff

--- app/main.go
+++ app/main.go
@@ -6,24 +6,31 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handlerConnection(conn)
 	}
+
+}
+
+func handlerConnection(conn net.Conn) {
+	defer conn.Close()
+
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	conn.Write([]byte(response))
 }
