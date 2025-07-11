a116aae4af658a58303c2e058eb1cb1f8e2ce8ad
Brandon <badamsva@gmail.com>
2025-07-10 21:39:27 -0400

    refactor: rename handlers.go into routers.go

--- app/internal/server/routers.go
+++ app/internal/server/routers.go
@@ -6,7 +6,7 @@ import "fmt"
 import "github.com/codecrafters-io/http-server-starter-go/app/internal/constants"
 
 // The string is the request string
-type HandlerFunc func(net.Conn, *constants.Request)
+type HandlerFunc func(net.Conn, *Request)
 
 // Router is a dictionary of strings and handler functions
 type Router struct {
@@ -17,11 +17,11 @@ func (r *Router) AssignHandler(path string, handler HandlerFunc) {
 	r.routes[path] = handler
 }
 
-func (r *Router) Serve(conn net.Conn, request *constants.Request) {
+func (r *Router) Serve(conn net.Conn, request *Request) {
 	// fmt.Println(r.routes, "routes")
 	for routePath, routeFunc := range r.routes {
 		// fmt.Println(request.UrlParts[1], "Route", routePath)
-		if request.UrlParts[1] == strings.Replace(routePath, "/", "", 1) {
+		if request.Path[1] == strings.Replace(routePath, "/", "", 1) {
 			fmt.Println("Running function for", routePath)
 			routeFunc(conn, request)
 			return
